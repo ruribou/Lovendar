026b7319fddf67f9ba63d99ac7a4ae6b131f0410
ruribou <ruribou@hotmail.com>
2025-09-22 14:50:08 +0900

    Initial Commit

---
+++ Lovendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lovendar
+//
+//  Created by Ryota Asai on 2025/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lovendar/LovendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovendarApp.swift
+//  Lovendar
+//
+//  Created by Ryota Asai on 2025/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LovendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
